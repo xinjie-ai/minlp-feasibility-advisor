51abfead940cb9b48f26f7226970ab1752a2f6fc
Xinjie Tong <xinjietong001@gmail.com>
2026-08-01 12:51:06 -0500

    Clean the repository

--- test_llm.py
+++
@@ -1,9 +0,0 @@
-# test_llm.py
-
-from llm_client import ask_llm
-
-response = ask_llm(
-    "Reply with only the word hello."
-)
-
-print(response)
--- test_qwen.py
+++
@@ -1,9 +0,0 @@
-# test_qwen.py
-
-from llm_client import ask_llm
-
-response = ask_llm(
-    "Reply with only the word hello."
-)
-
-print(repr(response))
